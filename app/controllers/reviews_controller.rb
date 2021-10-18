132fdc74db944ed94fea4e6687882bc01679cc7b
good39original <aoiumi442001@yahoo.co.jp>
2021-10-18 17:14:48 +0900

    レビューの投稿機能の作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
   def index
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id =current_user.id
+    if @review.save
+      redirect_to movie_shop_reviews_path(@review.movie_shop)
+    else
+      @movie_shop = MovieShop.find(params[:id])
+      render "movie_shops/show"
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:movie_shop_id, :score, :content)
+  end
 end
